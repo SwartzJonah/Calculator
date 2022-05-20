d573db1288623fce9a7d4a5c949eaa5022b49004
SwartzJonah <swartzjonah@gmail.com>
2022-05-20 09:59:02 -0400

    added basic math functions and an operate function

---
+++ javascript.js
@@ -0,0 +1,34 @@
+function add(numberOne, numberTwo){
+    return (numberOne + numberTwo);
+}
+
+function subtract(numberOne, numberTwo){
+    return (numberOne - numberTwo);
+}
+
+function multiply(numberOne, numberTwo){
+    return (numberOne * numberTwo);
+}
+
+function divide(numberOne, numberTwo){
+    return (numberOne/numberTwo);
+}
+
+function operate(operator, numberOne, numberTwo){
+    let solution = 0;
+    switch (operator){
+        case add:
+            solution = add(numberOne, numberTwo);
+            break;
+        case subtract:
+            solution = subtract(numberOne, numberTwo);
+            break;
+        case multiply:
+            solution = multiply(numberOne, numberTwo);
+            break;
+        case divide:
+            solution = divide(numberOne, numberTwo);
+            break;
+    }
+    return solution;
+}
