09787546c169791f572dc6ab63b04b4579797cde
SwartzJonah <swartzjonah@gmail.com>
2022-05-20 11:24:31 -0400

    starter state

--- javascript.js
+++ javascript.js
@@ -1,3 +1,45 @@
+//Declarations
+const problemDisplay = document.querySelector('#problemDisplay');
+const solutionDisplay = document.querySelector('#solutionDisplay');
+//Buttons
+const allClear = document.querySelector('#allClear');
+const justClear = document.querySelector('#justClear');
+const remainderButton = document.querySelector('#remainderButton');
+const divideButton = document.querySelector('#divideButton');
+const sevenButton = document.querySelector('#sevenButton');
+const eightButton = document.querySelector('#eightButton');
+const nineButton = document.querySelector('#nineButton');
+const timesButton = document.querySelector('#timesButton');
+const fourButton = document.querySelector('#fourButton');
+const fiveButton = document.querySelector('#fiveButton');
+const sixButton = document.querySelector('#sixButton');
+const minusButton = document.querySelector('#minusButton');
+const oneButton = document.querySelector('#oneButton');
+const twoButton = document.querySelector('#twoButton');
+const othreeButton = document.querySelector('#threeButton');
+const addButton = document.querySelector('#addButton');
+const zeroButton = document.querySelector('#zeroButton');
+const decimalButton = document.querySelector('#decimalButton');
+const negativeButton = document.querySelector('#negativeButton');
+const equalsButton = document.querySelector('#equalsButton');
+//Starter State
+let total = 0;
+let numberOne = 0;
+let numberTwo = 0;
+let operator = `+`;
+start();
+
+//Functions
+function start(){
+    problemDisplay.textContent = `${numberOne} ${operator} ${numberTwo}`;
+    solutionDisplay.textContent = `${total}`;
+}
+
+
+
+
+
+//Math Functions
 function add(numberOne, numberTwo){
     return (numberOne + numberTwo);
 }
